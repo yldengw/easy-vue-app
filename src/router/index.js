233fbbd70be7df813ea485df5ccc96d7077a01a5
dengyanan <dengyanan@cloud-star.com.cn>
2018-08-10 15:46:48 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from '../views/home.vue';
+import Lists from '../views/lists.vue';
+import Option from '../views/option.vue';
+import Detail from '../views/detail.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/lists', component: Lists },
+  { path: '/option', component: Option },
+  { path: '/detail', component: Detail },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: '/book/',
+  routes: routes});
+
+export default router;
